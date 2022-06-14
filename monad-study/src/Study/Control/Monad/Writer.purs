b2f96fd9658a9b8a9d2b0bb7e505ecbf63c0de5e
Kenji Suzuki <kenji.suzuki@uzabase.com>
2022-06-14 12:49:53 +0900

    map書いた

--- monad-study/src/Study/Control/Monad/Writer.purs
+++ monad-study/src/Study/Control/Monad/Writer.purs
@@ -11,7 +11,7 @@ derive instance newtypeWriter :: Newtype (Writer w a) _
 
 instance functorWriter :: Functor (Writer w) where
   map :: forall w a b. (a -> b) -> Writer w a -> Writer w b
-  map f (Writer w) = Writer ((\a w -> (f a) w) <$> w)
+  map f (Writer (Tuple a w)) = Writer (Tuple (f a) w)
 
 runWriter :: forall w a. Writer w a -> (Tuple a w)
 runWriter (Writer w) = w
