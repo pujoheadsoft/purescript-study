06df9beb2ba4d776b78323e6e4d409361c70099d
Pujo <pujoheadsoft@gmail.com>
2022-06-15 08:03:53 +0900

    add writer test & impl

--- monad-study/src/Study/Control/Monad/Writer.purs
+++ monad-study/src/Study/Control/Monad/Writer.purs
@@ -13,6 +13,14 @@ instance functorWriter :: Functor (Writer w) where
   map :: forall w a b. (a -> b) -> Writer w a -> Writer w b
   map f (Writer (Tuple a w)) = Writer (Tuple (f a) w)
 
+instance applyWriter :: Semigroup w => Apply (Writer w) where
+  apply :: forall a b w. Semigroup w => Writer w (a -> b) -> Writer w a -> Writer w b
+  apply (Writer (Tuple f w)) (Writer (Tuple a w')) = Writer (Tuple (f a) (w <> w'))
+
+instance applicativeWriter :: Monoid w => Applicative (Writer w) where
+  pure :: forall a w. Monoid w => a -> Writer w a
+  pure a = Writer (Tuple a mempty)
+
 tell :: forall a. a -> Writer a Unit
 tell a = Writer (Tuple unit a)
 
--- monad-study/test/Study/Control/Monad/WriterSpec.purs
+++ monad-study/test/Study/Control/Monad/WriterSpec.purs
@@ -1,14 +1,32 @@
 module Test.Study.Control.Monad.WriterSpec where
 
 import Prelude
+
 import Data.Tuple (Tuple(..))
-import Study.Control.Monad.Writer (runWriter, tell)
+import Study.Control.Monad.Writer (Writer(..), runWriter, tell)
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 
 spec :: Spec Unit
 spec = do
   describe "Writer" do
+    describe "runWriter" do
+      it "Writerの内容を取り出すことができる" do
+        runWriter (Writer (Tuple 100 "value")) `shouldEqual` (Tuple 100 "value") 
+
+    describe "Functorである" do
+      it "内容に関数を適用することができる" do
+        runWriter ((_ * 3) <$> Writer (Tuple 10 "X")) `shouldEqual` (Tuple 30 "X")
+
+    describe "Applyである" do
+      it "ログを蓄積しつつ、複数のWriterを引数にWriterの関数を適用することができる" do
+        runWriter (Writer (Tuple (+) "a") <*> Writer (Tuple 10 "b") <*> Writer (Tuple 20 "c")) `shouldEqual`  (Tuple 30 "abc")
+    
+    describe "Applicativeである" do
+      it "Writer型として返すことができる" do
+        runWriter (pure 10) `shouldEqual` (Tuple 10 "")
+
     describe "tell" do
       it "結果の値と、Writerに蓄積した値をTupleで取得することができる" do
         (Tuple unit "hello") `shouldEqual` runWriter (tell "hello")
+
