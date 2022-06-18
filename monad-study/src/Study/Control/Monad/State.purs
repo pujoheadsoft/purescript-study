c1740af2b2b90e9783dd0df2c490e0d4d017a863
Pujo <pujoheadsoft@gmail.com>
2022-06-18 12:40:15 +0900

    StateをFunctorにした

--- monad-study/src/Study/Control/Monad/State.purs
+++ monad-study/src/Study/Control/Monad/State.purs
@@ -3,7 +3,7 @@ module Study.Control.Monad.State where
 import Prelude
 
 import Data.Newtype (class Newtype)
-import Data.Tuple (Tuple, fst, snd)
+import Data.Tuple (Tuple(..), fst, snd)
 
 --
 -- s 実行前の状態
@@ -24,3 +24,9 @@ evalState s i = fst (runState s i)
 
 execState :: forall s a. State s a -> s -> s
 execState s i = snd (runState s i)
+
+instance functorState :: Functor (State s) where
+  map :: forall s a b. (a -> b) -> State s a -> State s b
+  map f m = State $ \s -> let 
+    (Tuple a s') = runState m s
+    in (Tuple (f a) s')
--- monad-study/test/Study/Control/Monad/StateSpec.purs
+++ monad-study/test/Study/Control/Monad/StateSpec.purs
@@ -11,16 +11,21 @@ spec :: Spec Unit
 spec = do
   describe "State" do
     let
-      fn = State $ \x -> (Tuple unit x)
+      fn = \x -> (Tuple 100 x)
     describe "runState" do
       it "値を取り出すことができる" do
         -- 関数自体の比較はできないため、関数の戻り値でassertする
-        (runState fn) "Value" `shouldEqual` (Tuple unit "Value")
+        runState (State fn) "Value" `shouldEqual` (Tuple 100 "Value")
     
     describe "evalState" do
       it "runStateで評価した結果の「値」だけを返す" do
-        (evalState fn) "value" `shouldEqual` unit
+        evalState (State fn) "value" `shouldEqual` 100
     
     describe "execState" do
       it "runStateで評価した結果の「状態」だけを返す" do
-        (execState fn) "value" `shouldEqual` "value"
+        execState (State fn) "value" `shouldEqual` "value"
+
+    describe "Functor" do
+      it "関数を適用することができる" do
+        runState ((_ * 10) <$> (State fn)) "Value" `shouldEqual` (Tuple 1000 "Value")
+
