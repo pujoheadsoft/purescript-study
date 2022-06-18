2660bdd977c515431c69b3e33bda95c9db418c4e
Pujo <pujoheadsoft@gmail.com>
2022-06-18 12:05:10 +0900

    StateにevalStateとexecStateを追加

--- monad-study/src/Study/Control/Monad/State.purs
+++ monad-study/src/Study/Control/Monad/State.purs
@@ -1,11 +1,26 @@
 module Study.Control.Monad.State where
 
-import Data.Newtype (class Newtype)
-import Data.Tuple (Tuple)
+import Prelude
 
+import Data.Newtype (class Newtype)
+import Data.Tuple (Tuple, fst, snd)
+
+--
+-- s 実行前の状態
+-- -> 
+-- (Tuple
+--   a 実行結果
+--   s 実行後の状態
+-- )
 newtype State s a = State (s -> (Tuple a s))
 
 derive instance newtypeState :: Newtype (State s a) _
 
 runState :: forall s a. State s a -> (s -> (Tuple a s))
 runState (State f) = f
+
+evalState :: forall s a. State s a -> s -> a
+evalState s i = fst (runState s i)
+
+execState :: forall s a. State s a -> s -> s
+execState s i = snd (runState s i)
--- monad-study/test/Study/Control/Monad/StateSpec.purs
+++ monad-study/test/Study/Control/Monad/StateSpec.purs
@@ -3,16 +3,24 @@ module Test.Study.Control.Monad.StateSpec where
 import Prelude
 
 import Data.Tuple (Tuple(..))
-import Study.Control.Monad.State (State(..), runState)
+import Study.Control.Monad.State (State(..), runState, evalState, execState)
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 
 spec :: Spec Unit
 spec = do
   describe "State" do
+    let
+      fn = State $ \x -> (Tuple unit x)
     describe "runState" do
       it "値を取り出すことができる" do
         -- 関数自体の比較はできないため、関数の戻り値でassertする
-        let
-          fn = State $ \x -> (Tuple unit x)
         (runState fn) "Value" `shouldEqual` (Tuple unit "Value")
+    
+    describe "evalState" do
+      it "runStateで評価した結果の「値」だけを返す" do
+        (evalState fn) "value" `shouldEqual` unit
+    
+    describe "execState" do
+      it "runStateで評価した結果の「状態」だけを返す" do
+        (execState fn) "value" `shouldEqual` "value"
