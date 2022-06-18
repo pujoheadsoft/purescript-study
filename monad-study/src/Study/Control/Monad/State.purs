b4759f48c4f1ac8a8d23a97cfbba2dcc22f5404f
Pujo <pujoheadsoft@gmail.com>
2022-06-18 11:04:58 +0900

    StateにrunStateを追加

--- monad-study/src/Study/Control/Monad/State.purs
+++ monad-study/src/Study/Control/Monad/State.purs
@@ -1,7 +1,11 @@
 module Study.Control.Monad.State where
 
-import Prelude
-
+import Data.Newtype (class Newtype)
 import Data.Tuple (Tuple)
 
 newtype State s a = State (s -> (Tuple a s))
+
+derive instance newtypeState :: Newtype (State s a) _
+
+runState :: forall s a. State s a -> (s -> (Tuple a s))
+runState (State f) = f
--- monad-study/test/Study/Control/Monad/StateSpec.purs
+++ monad-study/test/Study/Control/Monad/StateSpec.purs
@@ -1,3 +1,18 @@
 module Test.Study.Control.Monad.StateSpec where
 
 import Prelude
+
+import Data.Tuple (Tuple(..))
+import Study.Control.Monad.State (State(..), runState)
+import Test.Spec (Spec, describe, it)
+import Test.Spec.Assertions (shouldEqual)
+
+spec :: Spec Unit
+spec = do
+  describe "State" do
+    describe "runState" do
+      it "値を取り出すことができる" do
+        -- 関数自体の比較はできないため、関数の戻り値でassertする
+        let
+          fn = State $ \x -> (Tuple unit x)
+        (runState fn) "Value" `shouldEqual` (Tuple unit "Value")
