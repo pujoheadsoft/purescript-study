f9b58054179b4d088b4f09e912196c9d11f4ec48
Pujo <pujoheadsoft@gmail.com>
2022-06-21 07:54:35 +0900

    Free版Writerを作った。tellまで実装できてる。

---
+++ monad-study/src/Study/Control/Monad/Free/FreeWriter.purs
@@ -0,0 +1,32 @@
+module Study.Control.Monad.Free.FreeWriter where
+
+import Prelude
+
+import Data.Either (Either(..))
+import Data.Tuple (Tuple(..))
+import Study.Control.Monad.Free.Free (Free, liftF, resume)
+
+data Writer w a = Writer w a
+
+derive instance functorWriter :: Functor (Writer w)
+
+type FreeWriter w a = Free (Writer w) a
+
+writer :: forall w a. Tuple a w -> FreeWriter w a
+writer (Tuple a w) = liftF (Writer w a)
+
+runWriter :: forall w a. Monoid w => FreeWriter w a -> (Tuple a w)
+runWriter f = foldWriter (<>) mempty f
+
+foldWriter :: forall w a. (w -> w -> w) -> w -> FreeWriter w a -> Tuple a w
+foldWriter = loop
+  where
+  loop k w f = case resume f of
+    Left (Writer w' n) ->
+      loop k (k w w') n
+    Right a ->
+      (Tuple a w)
+
+
+tell :: forall w. Monoid w => w -> FreeWriter w Unit
+tell w = writer (Tuple unit w)
---
+++ monad-study/test/Study/Control/Monad/Free/FreeWriterSpec.purs
@@ -0,0 +1,66 @@
+module Test.Study.Control.Monad.Free.FreeWriterSpec where
+
+import Prelude
+
+import Data.Tuple (Tuple(..))
+import Study.Control.Monad.Free.FreeWriter (runWriter, writer, tell)
+import Test.Spec (Spec, describe, it)
+import Test.Spec.Assertions (shouldEqual)
+
+spec :: Spec Unit
+spec = do
+  describe "Free版Writer" do
+    describe "runWriter" do
+      it "Writerの内容を取り出すことができる" do
+        runWriter (writer (Tuple 100 "value")) `shouldEqual` (Tuple 100 "value") 
+
+    describe "Functorである" do
+      it "内容に関数を適用することができる" do
+        runWriter ((_ * 3) <$> writer (Tuple 10 "X")) `shouldEqual` (Tuple 30 "X")
+
+    describe "Applyである" do
+      it "ログを蓄積しつつ、複数のWriterを引数にWriterの関数を適用することができる" do
+        runWriter (writer (Tuple (+) "a") <*> writer (Tuple 10 "b") <*> writer (Tuple 20 "c")) `shouldEqual`  (Tuple 30 "abc")
+
+    describe "Applicativeである" do
+      it "Writer型として返すことができる" do
+        runWriter (pure 10) `shouldEqual` (Tuple 10 "")
+
+    describe "Bindである" do
+      it "第1引数の結果に第2引数の関数を適用させることができる" do
+        runWriter (bind (writer (Tuple 100 "value")) (\a -> pure (a * 4))) `shouldEqual` (Tuple 400 "value")
+
+    describe "tell" do
+      it "結果の値と、Writerに蓄積した値をTupleで取得することができる" do
+        runWriter (tell "hello") `shouldEqual` (Tuple unit "hello")
+
+      it "bindの中でtellを使うことができる" do
+        let 
+          actual = runWriter $ do 
+            tell "Hello"
+            tell "World"
+            pure 2022
+        actual `shouldEqual` (Tuple 2022 "HelloWorld")
+
+    -- describe "listen" do
+    --   it "引数で与えた式のログの書き込み結果を取得することができる" do
+    --     runWriter (listen (tell "hello")) `shouldEqual` (Tuple (Tuple unit "hello") "hello")
+
+    -- describe "listens" do
+    --   it "ログの取得結果に関数を適用することができる" do
+    --     runWriter (listens (_ <> "World") (tell "Hello")) `shouldEqual` (Tuple (Tuple unit "HelloWorld") "Hello")
+
+    -- describe "pass" do
+    --   it "値として「値と関数のTuple」を渡すことで、ログの内容にその関数を適用することができる" do
+    --     let
+    --       p = pass do 
+    --         tell "Hello"
+    --         pure $ Tuple 100 (_ <> "World")
+    --     runWriter p `shouldEqual` (Tuple 100 "HelloWorld")
+    
+    -- describe "censor" do
+    --   it "ログの内容に適用する関数を渡したWriterのログに適用することができる" do
+    --     -- passでやってることをもっと楽にできる
+    --     runWriter (censor (_ <> "World") (tell "Hello")) `shouldEqual` (Tuple unit "HelloWorld")
+        
+
