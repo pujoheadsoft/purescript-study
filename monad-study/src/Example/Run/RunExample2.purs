958a492761e8d88bb1b43ecde5c65b5e453b24e2
Kenji Suzuki <kenji.suzuki@uzabase.com>
2022-06-28 18:29:51 +0900

    wip

--- monad-study/src/Example/Run/RunExample2.purs
+++ monad-study/src/Example/Run/RunExample2.purs
@@ -2,8 +2,9 @@ module Example.Run.RunExample2 where
 
 import Prelude
 
+import Data.Either (Either(..))
 import Data.Functor.Variant (match, on)
-import Study.Control.Monad.Run.Run (Run, lift, run, send)
+import Study.Control.Monad.Run.Run (Run, lift, peel, run, send)
 import Type.Proxy (Proxy(..))
 import Type.Row (type (+))
 
@@ -30,3 +31,13 @@ handleUserPort (FindUserById id _) = User {id: id, name: "dummy"}
 handleUserPort (FindUserByName name _) = User {id: "dummy" , name: name}
 
 -- runUserPort :: forall r a. Run (USER_PORT + r) a -> Run r User
+-- runUserPort :: forall r a b. (UserPort a -> b) -> Run (USER_PORT + r ) b -> Run r b
+-- runUserPort f r = case peel r of
+--   Left a -> case on _userPort Left Right a of
+--     Left a' -> pure (f a')
+--     Right a' -> send a' >>= \r' -> runUserPort r'
+--   Right a ->
+--     pure a
+
+-- main = do
+--   runUserPort (findUserById "xxx")
