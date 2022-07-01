81cc74779f5e67ff9481bb81dd4ecfb8a81d4325
Pujo <pujoheadsoft@gmail.com>
2022-07-01 20:11:12 +0900

    exampleで継続渡しができるようになった

--- monad-study/src/Example/Run/RunExample2.purs
+++ monad-study/src/Example/Run/RunExample2.purs
@@ -3,52 +3,49 @@ module Example.Run.RunExample2 where
 import Prelude
 
 import Data.Either (Either(..))
-import Data.Functor.Variant (match, on)
-import Debug (trace, traceM)
+import Data.Functor.Variant (on)
+import Debug (traceM)
 import Effect (Effect)
-import Effect.Console (log)
-import Study.Control.Monad.Run.Run (Run, extract, lift, peel, run, send)
+import Study.Control.Monad.Run.Run (Run, extract, lift, peel, send)
 import Type.Proxy (Proxy(..))
 import Type.Row (type (+))
 
-newtype User = User {id :: String, name :: String}
+type User = {id :: String, name :: String}
 
-data UserPort :: forall k. k -> Type
 data UserPort a
-  = FindUserById String
-  | FindUserByName String
+  = FindUserById String (User -> a)
+  | FindUserByName String (User -> a)
 
 derive instance functorUserPort :: Functor UserPort
 
-type USER_PORT :: forall k. Row (k -> Type) -> Row (k -> Type)
 type USER_PORT r = (userPort :: UserPort | r)
 
 _userPort = Proxy :: Proxy "userPort"
 
-findUserById :: forall a r. String -> Run (USER_PORT + r) a
-findUserById id = lift _userPort (FindUserById id)
+findUserById :: forall r. String -> Run (USER_PORT + r) User
+findUserById id = lift _userPort (FindUserById id identity)
 
-findUserByName :: forall a r. String -> Run (USER_PORT + r) a
-findUserByName name = lift _userPort (FindUserByName name)
+findUserByName :: forall r. String -> Run (USER_PORT + r) User
+findUserByName name = lift _userPort (FindUserByName name identity)
 
-handleUserPort :: forall a. UserPort a -> User
-handleUserPort (FindUserById id) = User {id: id, name: "dummy"}
-handleUserPort (FindUserByName name) = User {id: "dummy" , name: name}
+handleUserPort :: forall a. UserPort a -> a
+handleUserPort (FindUserById id reply) = reply {id: id, name: "dummy"}
+handleUserPort (FindUserByName name reply) = reply {id: "dummy" , name: name}
 
 runUserPort :: forall r a. Run (USER_PORT + r) User -> Run a User
 runUserPort r = case peel r of
   Left a -> case on _userPort Left Right a of
-    Left a' -> pure (handleUserPort a')
+    Left a' -> runUserPort (handleUserPort a')
     Right a' -> send a' >>= \r' -> runUserPort r'
   Right a ->
     pure a
 
-program :: forall a r. String -> Run (USER_PORT + r) a
-program name = do
-  user <- findUserByName name
-  findUserById user.id
+program :: forall r. String -> Run (USER_PORT + r) User
+program id = do
+  user <- findUserById id
+  findUserByName user.id
 
 main :: Effect Unit
 main = do
-   traceM(extract (runUserPort (program "userName")))
+  traceM(extract (runUserPort (program "userId")))
    
