b8bafdaa56b8970f8f0da5585fa4c281ea4b1aa3
Pujo <pujoheadsoft@gmail.com>
2022-07-04 08:40:26 +0900

    WIP

--- monad-study/src/Example/Run/Example2.purs
+++ monad-study/src/Example/Run/Example2.purs
@@ -12,26 +12,24 @@ import Type.Row (type (+))
   DIできるようにしたい
   gatewayが使うdriverをDIするなど
 -}
-
-type User = {}
+type UserDomain = {}
 
 class Monad m <= UserGateway m where
-  findById :: String -> m User
+  findById :: String -> m UserDomain
 
 -- find user ---------------------------------------------
+type User = {id :: String, name :: String}
+
 data FindUser a = FindUserById String (User -> a)
-
 derive instance functorFindUser :: Functor FindUser
-
 type FIND_USER r = (findUser :: FindUser | r)
-
 _findUser = Proxy :: Proxy "findUser"
 
 findUserById :: forall r. String -> Run (FIND_USER + r) User
 findUserById id = lift _findUser (FindUserById id identity) 
 
 handleFindUser :: forall a. FindUser a -> a
-handleFindUser (FindUserById id reply) = reply {}
+handleFindUser (FindUserById id reply) = reply {id: id, name: "dummy"}
 
 runFindUser :: 
   forall r a
@@ -41,3 +39,23 @@ runFindUser ::
 runFindUser f r = runPure (\v -> on _findUser (Loop <<< f) Done v) r
 
 -- find user option ----------------------------------------
+type UserOption = {userId :: String, optionId :: String, label :: String}
+
+data FindUserOption a = FindUserOptionById String (UserOption -> a)
+derive instance functorFindUserOption :: Functor FindUserOption
+type FIND_USER_OPTION r = (findUserOption :: FindUserOption | r)
+_findUserOption = Proxy :: Proxy "findUserOption"
+
+findUserOptionById :: forall r. String -> Run (FIND_USER_OPTION + r) UserOption
+findUserOptionById id = lift _findUserOption (FindUserOptionById id identity) 
+
+handleFindUserOption :: forall a. FindUserOption a -> a
+handleFindUserOption (FindUserOptionById id reply) = reply {userId: id, optionId: "dummy", label: "dummy"}
+
+runFindUserOption :: 
+  forall r a
+   . (FindUserOption (Run (FIND_USER_OPTION + r) a) -> Run (FIND_USER_OPTION + r) a)
+  -> Run (FIND_USER_OPTION + r) a
+  -> Run r a
+runFindUserOption f r = runPure (\v -> on _findUserOption (Loop <<< f) Done v) r
+-------------------------------------------------------
