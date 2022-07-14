03f97fbe8f6f78a730816761e97a5418c6899427
Pujo <pujoheadsoft@gmail.com>
2022-07-15 07:59:14 +0900

    WIP

--- monad-study/src/CleanArchitecture/Driver/ArticleApiDriver.purs
+++ monad-study/src/CleanArchitecture/Driver/ArticleApiDriver.purs
@@ -11,7 +11,7 @@ import Data.Functor.Variant (on)
 import Data.Maybe (Maybe(..))
 import Effect.Aff (Aff)
 import Simple.JSON (readJSON)
-import Study.Control.Monad.Run.Run (AFF, Run, interpret, lift, liftAff, runBaseAff, send)
+import Study.Control.Monad.Run.Run (AFF, Run, extract, interpret, lift, liftAff, runBaseAff, send)
 import Type.Proxy (Proxy(..))
 import Type.Row (type (+))
 
@@ -27,6 +27,7 @@ type ArticleJson = {title :: String, body :: String, author :: String}
 data ArticleDriver a = 
    FindArticlesByTitle String (Array ArticleIndexJson -> a)
  | FindArticlesByIds (Array String) (Array ArticleJson -> a)
+ | FindArticleById String (Maybe ArticleJson -> a)
 
 derive instance functorArticleDriver :: Functor ArticleDriver
 _articleDriver = Proxy :: Proxy ("articleDriver")
@@ -41,15 +42,19 @@ findArticlesByTitle title = lift _articleDriver $ FindArticlesByTitle title iden
 findArticlesByIds :: forall r. Array String -> Run (ARTICLE_DRIVER + r) (Array ArticleJson)
 findArticlesByIds ids = lift _articleDriver $ FindArticlesByIds ids identity
 
+findArticleById :: forall r. String -> Run (ARTICLE_DRIVER + r) (Maybe ArticleJson)
+findArticleById id = lift _articleDriver $ FindArticleById id identity
+
 handleArticleDriver :: forall a. ArticleDriver a -> a
 handleArticleDriver = case _ of
   (FindArticlesByTitle title next) -> next [{id: ""}]
   (FindArticlesByIds ids next) -> next $ [{title: "", body: "", author: ""}]
+  (FindArticleById id next) -> next $ Just {title: "", body: "", author: ""}
 
 -- こいつらをどうくっつけたものか
 
-xxx :: String -> Aff (Array ArticleIndexJson)
-xxx title = do
+_findArticlesByTitle :: String -> Aff (Array ArticleIndexJson)
+_findArticlesByTitle title = do
   res <- AX.get ResponseFormat.string $ "http://article-api/articles?title" <> title
   case res of
     Left err -> do
@@ -60,15 +65,15 @@ xxx title = do
         Left e -> pure [] -- pure $ Left $ show e -- do log $ "Can't parse JSON. " <> show e
         Right (r :: Array ArticleIndexJson) -> pure r -- pure $ Right r -- do log $ "article id is: " <> show r.id
 
-yyy ∷ Array String → Aff (Array ArticleJson)
-yyy ids = do
+_findArticlesByIds ∷ Array String → Aff (Array ArticleJson)
+_findArticlesByIds ids = do
   let
     urls = (\id -> "http://article-api/articles/" <> id) <$> ids
-  responses <- parTraverse zzz urls
+  responses <- parTraverse _findArticleById urls
   pure (catMaybes responses)
 
-zzz :: String -> Aff (Maybe ArticleJson)
-zzz id = do
+_findArticleById :: String -> Aff (Maybe ArticleJson)
+_findArticleById id = do
   res <- AX.get ResponseFormat.string $ "http://article-api/articles/" <> id
   case res of
     Left err -> pure Nothing
@@ -77,13 +82,17 @@ zzz id = do
         Left e -> pure Nothing
         Right (r :: ArticleJson) -> pure $ Just r
 
+-- 実値を返すhandler
 handleDriver :: forall r. ArticleDriver ~> Run (AFF + r)
 handleDriver = case _ of
   FindArticlesByTitle title next -> do
-    articles <- liftAff $ xxx title
+    articles <- liftAff $ _findArticlesByTitle title
     pure $ next articles
   FindArticlesByIds ids next -> do
-    article <- liftAff $ yyy ids
+    article <- liftAff $ _findArticlesByIds ids
+    pure $ next article
+  FindArticleById id next -> do
+    article <- liftAff $ _findArticleById id
     pure $ next article
 
 -- 固定値を返すmock
@@ -92,30 +101,33 @@ handleDriverMock :: forall r. ArticleDriver ~> Run (AFF + r)
 handleDriverMock = case _ of
   FindArticlesByTitle _ next -> pure $ next [{id: ""}]
   FindArticlesByIds _ next -> pure $ next $ [{title: "", body: "", author: ""}]
+  FindArticleById _ next -> pure $ next $ Just {title: "", body: "", author: ""}
 
 runDriver :: forall r. Run (AFF + ARTICLE_DRIVER + r) ~> Run (AFF + r)
 runDriver = interpret (on _articleDriver handleDriver send)
 
 -- ここいらはgatewayから呼ぶやつかなあ
-program1 :: forall r. String -> Run (AFF + r) (Array ArticleIndexJson)
-program1 title = findArticlesByTitle title # runDriver
+runFindArticlesByTitle :: forall r. String -> Run (AFF + r) (Array ArticleIndexJson)
+runFindArticlesByTitle title = findArticlesByTitle title # runDriver
 
-program2 :: forall r. Array String -> Run (AFF + r) (Array ArticleJson)
-program2 ids = findArticlesByIds ids # runDriver
+runFindArticlesByIds :: forall r. Array String -> Run (AFF + r) (Array ArticleJson)
+runFindArticlesByIds ids = findArticlesByIds ids # runDriver
+
+runFindArticleById :: forall r. String -> Run (AFF + r) (Maybe ArticleJson)
+runFindArticleById id = findArticleById id # runDriver
        
-program3 :: forall r. String -> Run (ARTICLE_DRIVER + r) (Array ArticleJson)
-program3 title = do
+program1 :: forall r. String -> Run (ARTICLE_DRIVER + r) (Array ArticleJson)
+program1 title = do
   indexes <- findArticlesByTitle title
   articles <- findArticlesByIds $ (\index -> index.id) <$> indexes
   pure articles
 
 
 main :: Aff (Array ArticleIndexJson)
-main = runBaseAff $ program1 "title"
+main = runBaseAff $ runFindArticlesByTitle "title"
 
-
--- program2 :: Effect Unit
--- program2 = program # runCont go done
+-- runFindArticlesByIds :: Effect Unit
+-- runFindArticlesByIds = program # runCont go done
 --   where
 --   go = match
 --     { log: \(Log str cb) -> Console.log str *> cb
--- monad-study/test/CleanArchitecture/Gateway/ArticleGatewaySpec.purs
+++ monad-study/test/CleanArchitecture/Gateway/ArticleGatewaySpec.purs
@@ -6,10 +6,10 @@ import CleanArchitecture.Gateway.ArticleGateway as Gateway
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 
-spec :: Spec Unit
-spec = do
-  describe "UserUsecase" do
-    it "指定したIDのユーザ情報を表示することができる" do
-      let
-        expected = [{title: "title1", body: "body2", author: "author3"}]
-      Gateway.findArticlesByTitle "" `shouldEqual` expected
+-- spec :: Spec Unit
+-- spec = do
+--   describe "UserUsecase" do
+--     it "指定したIDのユーザ情報を表示することができる" do
+--       let
+--         expected = [{title: "title1", body: "body2", author: "author3"}]
+--       Gateway.findArticlesByTitle "" `shouldEqual` expected
