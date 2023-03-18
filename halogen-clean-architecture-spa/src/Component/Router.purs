2aea8903a460a1bfd2d7661fa6dbc62b37227b06
Pujo <pujoheadsoft@gmail.com>
2023-03-18 10:37:36 +0900

    WIP

--- halogen-clean-architecture-spa/src/Component/Router.purs
+++ halogen-clean-architecture-spa/src/Component/Router.purs
@@ -2,11 +2,18 @@ module Component.Router where
 
 import Prelude
 
+import AppM (AppM(..))
 import Component.Utils (OpaqueSlot)
+import Control.Monad.State.Class (class MonadState)
 import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple, snd)
+import HState (STATE, modify2, modify3, runState)
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
+import Run (Run(..), extract)
+import Run.Reader (READER, ask, runReader)
+import Type.Row (type (+))
 
 type Article = {
   title :: String
@@ -17,7 +24,7 @@ type State
 
 data Action
   = Initialize
-  | Search
+  | Update
 
 type ChildSlots
   = ( home :: OpaqueSlot Unit )
@@ -28,12 +35,10 @@ component =
   H.mkComponent
     { initialState: \_ -> { article: {title: ""} }
     , render
-    , eval:
-        H.mkEval
-          $ H.defaultEval
-              { initialize = Just Initialize,
-              handleAction = _handleAction
-              }
+    , eval: H.mkEval $ H.defaultEval { 
+        initialize = Just Initialize,
+        handleAction = _handleAction
+      }
     }
   where
   render :: forall cs. State -> H.ComponentHTML Action cs m
@@ -41,12 +46,52 @@ component =
     HH.div_
       [
         HH.button
-          [ HE.onClick \_ -> Search ]
-          [ HH.text "Search" ]
+          [ HE.onClick \_ -> Update ]
+          [ HH.text "更新" ]
        ,HH.text article.title
       ]
-
+  
   _handleAction :: Action -> H.HalogenM State Action ChildSlots Void m Unit
   _handleAction = case _ of
     Initialize -> H.modify_ \state -> state { article = { title: "ニュース" } }
-    Search -> H.modify_ \state -> state { article = { title: "検索されたニュース" } }
+    --Update -> H.modify_ \state -> state { article = { title: "ニュース" } }
+    Update -> xxxx
+    
+xxxx :: forall m. MonadState State m => m Unit
+xxxx = do
+  _update "新しいニュース"
+    # runState { article: { title: "ニュース" } }
+    # extract
+    # snd
+
+type ArticlePort = {
+  find :: forall r. Run (READER String + r) Article
+}
+
+type ArticlePresenter = {
+  update :: forall r m. MonadState State m => String -> Run ( STATE State + r ) (m Unit)
+}
+
+createPort :: ArticlePort
+createPort = {
+  find: _find
+}
+
+createPresenter :: ArticlePresenter
+createPresenter = {
+  update: _update
+}
+
+_find :: forall r. Run (READER String + r) Article
+_find = do
+  value <- ask
+  pure {title: value}
+
+_update :: forall r m. MonadState State m => String -> Run ( STATE State + r ) (m Unit)
+_update title = do
+  modify3 \state -> state { article = { title: title } }
+
+usecase :: forall m r. MonadState State m => ArticlePort -> ArticlePresenter -> Run (READER String + STATE State + r) (m Unit)
+usecase port presenter = do
+  article <- port.find
+  presenter.update article.title
--- halogen-clean-architecture-spa/src/Main.purs
+++ halogen-clean-architecture-spa/src/Main.purs
@@ -2,11 +2,8 @@ module Main where
 
 import Prelude
 
-import App.Button as Button
 import Component.Router as Router
 import Effect (Effect)
-import Effect.Aff (launchAff_)
-import Halogen as H
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
 
@@ -14,7 +11,7 @@ main :: Effect Unit
 main = HA.runHalogenAff do
   body <- HA.awaitBody
   runUI Router.component unit body
-  
+
   -- runUI Button.component unit body
   -- void $ liftEffect $ matchesWith (parse routeCodec) \old new ->
   --   when (old /= Just new) do
