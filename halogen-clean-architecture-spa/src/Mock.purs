f48cbd9bc672da8285b35d96c94ef388417c84d6
Pujo <pujoheadsoft@gmail.com>
2023-03-26 18:34:59 +0900

    wip: mock

--- halogen-clean-architecture-spa/src/Mock.purs
+++ halogen-clean-architecture-spa/src/Mock.purs
@@ -64,6 +64,64 @@ hoge a1 b1 r =
     in r)
   }
 
+
+mock1 :: forall a1 r. Eq a1 => Show a1 => a1 -> r -> Mock (a1 -> r)
+mock1 a1 r = 
+  let
+    s = store unit
+  in { args: s.args, fn: (\a2 -> 
+    let
+      _ = s.save $ VerifyArg {expected: show a1, actual: show a2, eqResult: a1 == a2}
+    in r)
+  }
+
+mock2 :: forall a1 b1 r. Eq a1 => Show a1 => Eq b1 => Show b1 => a1 -> b1 -> r -> Mock (a1 -> b1 -> r)
+mock2 a1 b1 r = 
+  let
+    s = store unit
+  in { args: s.args, fn: (\a2 b2 -> 
+    let
+      _ = s.save $ VerifyArg {expected: show a1, actual: show a2, eqResult: a1 == a2}
+      _ = s.save $ VerifyArg {expected: show b1, actual: show b2, eqResult: b1 == b2}
+    in r)
+  }
+
+mock3 :: forall a1 b1 c1 r. Eq a1 => Show a1 => Eq b1 => Show b1 => Eq c1 => Show c1 => a1 -> b1 -> c1 -> r -> Mock (a1 -> b1 -> c1 -> r)
+mock3 a1 b1 c1 r = 
+  let
+    s = store unit
+  in { args: s.args, fn: (\a2 b2 c2 -> 
+    let
+      _ = s.save $ VerifyArg {expected: show a1, actual: show a2, eqResult: a1 == a2}
+      _ = s.save $ VerifyArg {expected: show b1, actual: show b2, eqResult: b1 == b2}
+      _ = s.save $ VerifyArg {expected: show c1, actual: show c2, eqResult: c1 == c2}
+    in r)
+  }
+
+--m = moge 1 "2" true :: Mock (Int -> String -> Boolean)
+
+{-
+  aaからddまで合成していくと引数を増やすことができる
+  構造としては、渡した関数をラップする引数を一つ増やした関数を返す構造になっている
+  (a) -> (b -> a)
+  これを再帰すれば可変長引数は実現できる
+-}
+-- これは別になくてもいい
+aa :: forall a. a -> a
+aa a = a
+
+-- こいつが基本
+-- こいつと↓のやつを動的に合成していければよさそう
+-- bb >>> cc とすると引数を3つとる関数になる
+bb :: forall a1 a2. a1 -> (a2 -> a1)
+bb a1 = (\a2 -> a1)
+
+cc :: forall a1 a2 a3. (a2 -> a1) -> (a3 -> a2 -> a1)
+cc a2Toa1 = (\a3 -> a2Toa1)
+
+dd :: forall a1 a2 a3 a4. (a3 -> a2 -> a1) -> (a4 -> a3 -> a2 -> a1)
+dd a3a2a1 = (\a4 -> a3a2a1)
+
 main :: Effect Unit
 main = do
   let
