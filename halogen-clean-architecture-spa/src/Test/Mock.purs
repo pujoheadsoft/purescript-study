ef886b466471d81d201dd1bcde7f4cb6e3c4c221
Pujo <pujoheadsoft@gmail.com>
2023-04-09 11:09:08 +0900

    wip

--- halogen-clean-architecture-spa/src/Test/Mock.purs
+++ halogen-clean-architecture-spa/src/Test/Mock.purs
@@ -6,6 +6,7 @@ module Test.Mock
   mock,
   MockArgs,
   verify,
+  verifyCount,
   fun,
   Cons(..),
   (:)
@@ -17,7 +18,8 @@ import Prelude
 import Control.Monad.Error.Class (class MonadThrow)
 import Data.Array (filter, foldl, length)
 import Data.String (joinWith)
-import Effect.Exception (Error)
+import Effect.Exception (Error, throw)
+import Effect.Unsafe (unsafePerformEffect)
 import Test.Spec.Assertions (fail)
 
 newtype EqResult = EqResult { expected :: String, actual :: String, result :: Boolean}
@@ -33,6 +35,15 @@ newtype Mock a = Mock {
   results :: EqResults
 }
 
+derive instance mockFunctor :: Functor Mock
+instance applicativeMock :: Applicative Mock where
+  pure a = Mock {fn: a, results: []}
+instance applyMock :: Apply Mock where
+  apply (Mock {fn: a2b}) (Mock {fn: a, results}) = Mock {fn: a2b a, results: results}
+instance bindMock :: Bind Mock where
+  bind (Mock {fn}) f = f fn
+instance monadMock :: Monad Mock
+
 class MockBuilder a b r | a -> b, a -> r, b -> r where
   thenReturn :: a -> b -> Mock r
 
@@ -45,23 +56,29 @@ foreign import store :: Unit -> EqResultsStore
 
 newtype MockArgs a = MockArgs a
 
-instance instanceMockArgs3 :: (Show a, Eq a, Show b, Eq b, Show c, Eq c) => MockBuilder (MockArgs (Cons a (Cons b c))) r (a -> b -> c -> r) where
+instance instanceMockArgs3 :: (Show a, Eq a, Show b, Eq b, Show c, Eq c) => MockBuilder (MockArgs (a : b : c)) r (a -> b -> c -> r) where
   thenReturn (MockArgs (Cons a (Cons b c))) r = store unit # build
-    where build s = Mock { results: s.results, fn: (\a2 b2 c2 -> r `const` storeEqResults s [eqResult a a2, eqResult b b2, eqResult c c2]) }
+    where build s = Mock { results: s.results, fn: (\a2 b2 c2 -> r `const` validateWithStoreEqResults [eqResult a a2, eqResult b b2, eqResult c c2] s) }
 else
-instance instanceMockArgs2 :: (Show a, Eq a, Show b, Eq b) => MockBuilder (MockArgs (Cons a b)) r (a -> b -> r) where
+instance instanceMockArgs2 :: (Show a, Eq a, Show b, Eq b) => MockBuilder (MockArgs (a : b)) r (a -> b -> r) where
   thenReturn (MockArgs (Cons a b)) r = store unit # build
-    where build s = Mock { results: s.results, fn: (\a2 b2 -> r `const` storeEqResults s [eqResult a a2, eqResult b b2]) }
+    where build s = Mock { results: s.results, fn: (\a2 b2 -> r `const` validateWithStoreEqResults [eqResult a a2, eqResult b b2] s) }
 else
 instance instanceMockArgs1 :: (Show a, Eq a) => MockBuilder (MockArgs a) r (a -> r) where
   thenReturn (MockArgs a) r = store unit # build
-    where build s = Mock { results: s.results, fn: (\a2 -> r `const` storeEqResults s [eqResult a a2]) }
+    where build s = Mock { results: s.results, fn: (\a2  -> r `const` validateWithStoreEqResults [eqResult a a2] s) }
 
 eqResult :: forall a. Eq a => Show a => a -> a -> EqResult
 eqResult a1 a2 = EqResult {expected: show a1, actual: show a2, result: a1 == a2}
 
-storeEqResults :: EqResultsStore -> EqResults -> Unit
-storeEqResults s = foldl (\_ e -> s.store e) unit
+validateWithStoreEqResults :: EqResults -> EqResultsStore -> Unit
+validateWithStoreEqResults = storeEqResults <<< valdateEqResults
+
+valdateEqResults :: EqResults -> EqResults
+valdateEqResults results = if (isAnyUnMatch results) then error $ message results else results
+
+storeEqResults :: EqResults -> EqResultsStore -> Unit
+storeEqResults results s = foldl (\_ e -> s.store e) unit results
 
 mock :: forall a. a -> MockArgs a
 mock = MockArgs
@@ -71,11 +88,20 @@ fun (Mock {fn}) = fn
 
 verify :: forall a m. MonadThrow Error m => Mock a -> m Unit
 verify (Mock {results}) = 
-  let
-    isAllMatch = all (\(EqResult {result}) -> result) results
-  in 
-  if isAllMatch then pure unit
+  if 0 == length results then fail "Function has never been called"
+  else if all (\(EqResult {result}) -> result) results then pure unit
   else fail $ message results
+
+verifyCount :: forall a m. MonadThrow Error m => Mock a -> Int -> m Unit
+verifyCount (Mock {results}) count =
+  if count == length results then pure unit
+  else fail $ joinWith "\n" ["Function was not called the expected number of times.",  "expected: " <> show count, "but was : " <> show (length results)]
+
+isAllMatch :: EqResults -> Boolean
+isAllMatch = all (\(EqResult {result}) -> result)
+
+isAnyUnMatch :: EqResults -> Boolean
+isAnyUnMatch r = isAllMatch r == false
 
 all :: forall a. (a -> Boolean) -> Array a -> Boolean
 all fn arr = (length $ filter fn arr) == (length arr)
@@ -85,9 +111,12 @@ all fn arr = (length $ filter fn arr) == (length arr)
   expected: 1, "2", 3
   but was : 1, "1", 1
 -}
-message :: Array EqResult -> String
+message :: EqResults -> String
 message arr = 
   let
     expecteds = arr <#> (\(EqResult arg) -> arg.expected) # joinWith ", "
     actuals = arr <#> (\(EqResult arg) -> arg.actual) # joinWith ", "
   in joinWith "\n" ["Function was not called with expected arguments.",  "expected: " <> expecteds, "but was : " <> actuals]
+
+error :: forall a. String -> a
+error = unsafePerformEffect <<< throw
--- halogen-clean-architecture-spa/test/Test/MockSpec.purs
+++ halogen-clean-architecture-spa/test/Test/MockSpec.purs
@@ -1,28 +1,57 @@
 module Test.MockSpec where
 
 import Prelude
+
 import Component.State (State)
+import Control.Monad.Error.Class (class MonadError)
 import Control.Monad.State (class MonadState, StateT, runStateT)
 import Data.Identity (Identity)
 import Domain.Article (Article)
 import Effect.Aff (Aff)
-import Test.Mock (Mock, fun, mock, thenReturn, verify, (:))
+import Effect.Exception (Error)
+import Test.Mock (class MockBuilder, Mock, MockArgs, fun, mock, thenReturn, verify, verifyCount, (:))
 import Test.Spec (Spec, describe, it)
-import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Assertions (expectError, shouldEqual)
 
+xxxx :: forall r fn args. MockBuilder (MockArgs args) r fn => args -> r -> Mock fn
+xxxx = thenReturn <<< mock
 
 spec :: Spec Unit
 spec = do
-  describe "Mock Test" do
+  describe "任意の引数に対して任意の値を返す関数を生成することができる" do
+    it "引数が1つの関数を生成できる" do
+      let
+        m = mock "a" `thenReturn` 100
+      (fun m) "a" `shouldEqual` 100
+  
+  -- describe "期待する引数でない引数で呼び出した場合failになる" do
+  --   it "引数が1つの場合" do
+  --     let
+  --       m = mock "a" `thenReturn` 100
+  --     (fun m) "b" `shouldEqual` 100
+  --   it "引数が2つの場合" do
+  --     let
+  --       m = mock (1 : "a") `thenReturn` 100
+  --     (fun m) 1 "b" `shouldEqual` 100
+
+  describe "特定の引数で呼び出されたことを検証することができる" do
     it "mock" do
       let
         m = mock (1 : "2" : 3) `thenReturn` 100
+        _ = (fun m) 1 "2" 3
       verify m
+
+    it "一度も呼び出さない状態で検証をするとfailになる" do
+      let
+        m = mock 1 `thenReturn` 100
+      expectError $ verify m
     
     it "Monadを返すことができる1" do
       let
         m :: Mock (Int -> Identity String)
         m = mock 1 `thenReturn` pure "hoge"
+
+        _ = (fun m) 1
       verify m
     
     it "Monadを返すことができる2" do
@@ -41,3 +70,34 @@ spec = do
         f = fun (updateMock :: Mock (String -> StateT State Aff Unit))
       _ <- runStateT (f "新しいtitle") {article: {title: "Dummy"}} 
       verify (updateMock :: Mock (String -> StateT State Aff Unit))
+
+  describe "呼び出し回数を検証することができる" do
+    it "呼び出された回数を検証することができる(0回)" do
+      let
+        m = mock 1 `thenReturn` 100
+      verifyCount m 0
+
+    -- it "呼び出された回数を検証することができる(複数回)" do
+    --   let
+    --     m = mock (1 : 2)`thenReturn` 100
+    --     _ = (fun m) 1 2
+    --     _ = (fun m) 1 2
+    --   verifyCount m 2
+  
+  {-
+    m = mock [(1 : 2), (2 : 3)] `thenReturn` ["r1", "r2"]
+    multiMock [(1 : 2) `thenReturn` r1, (2 : 3) `thenReturn` r2]
+    do
+      mock (1 : 2) `thenReturn` "r1"
+      mock (2 : 3) `thenReturn` "r2"
+  -}
+  describe "一つのMockで複数の引数の組み合わせに対応できる" do
+    it "" do
+      let
+        m1 :: Mock (Int -> Int -> String)
+        m1 = mock (1 : 2) `thenReturn` "r1"
+
+        m2 :: Mock (Int -> Int -> String)
+        m2 = multiMock [(1 : 2) `thenReturn` "r1",
+                        (2 : 3) `thenReturn` "r2"]
+      "" `shouldEqual` ""
