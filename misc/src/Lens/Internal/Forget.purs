2f1011b0680ca514812c7c4e839d0fd52183521b
Pujo <pujoheadsoft@gmail.com>
2023-06-26 20:42:06 +0900

    wip lens

--- misc/src/Lens/Internal/Forget.purs
+++ misc/src/Lens/Internal/Forget.purs
@@ -2,15 +2,10 @@ module Lens.Internal.Forget where
 
 import Prelude
 
-import Data.Const (Const(..))
-import Data.Either (Either(..), either)
-import Data.Newtype (class Newtype, alaF)
+import Data.Newtype (class Newtype)
 import Data.Profunctor (class Profunctor)
-import Data.Profunctor.Choice (class Choice)
-import Data.Profunctor.Cochoice (class Cochoice)
 import Data.Profunctor.Strong (class Strong)
-import Data.Tuple (fst, snd)
-import Lens.Internal.Wander (class Wander)
+import Data.Tuple (Tuple, fst, snd)
 
 
 -- | `b`の値を忘れ、`a`を返す（累積する）Profunctor
@@ -26,7 +21,8 @@ derive instance newtypeForget :: Newtype (Forget r a b) _
 
 instance profunctorForget :: Profunctor (Forget r) where
   -- dimap の (c -> d) の部分がない。
-  -- Forgetは Profunctorの p a c, p b d における c や d がないからこの実装でいける
+  -- Forgetは Profunctorの p a c, p b d における c や d を無視しているからこの実装でいける
+  dimap :: forall a b c d. (a -> b) -> (c -> d) -> Forget r b c -> Forget r a d
   dimap f _ (Forget z) = Forget (z <<< f)
 
 -- instance choiceForget :: Monoid r => Choice (Forget r) where
@@ -34,7 +30,10 @@ instance profunctorForget :: Profunctor (Forget r) where
 --   right (Forget z) = Forget (either mempty z)
 
 instance strongForget :: Strong (Forget r) where
-  first (Forget z) = Forget (z <<< fst)
+  first :: forall a b c. Forget r a b -> Forget r (Tuple a c) (Tuple b c)
+  first (Forget z) = Forget (z <<< fst) -- Tuple b c は無視されて Tuple a c の a を fst で取って (a -> r) 渡している
+
+  second :: forall a b c. Forget r b c -> Forget r (Tuple a b) (Tuple a c)
   second (Forget z) = Forget ( z <<< snd)
 
 -- instance cochoiceForget :: Cochoice (Forget r) where
--- misc/src/Lens/Lens.purs
+++ misc/src/Lens/Lens.purs
@@ -1,10 +1,12 @@
 module Lens.Lens where
 
+import Prelude
 
-import Lens.Types (Lens)
 import Data.Profunctor (dimap)
-import Data.Profunctor.Strong (first)
+import Data.Profunctor.Strong (class Strong, first)
 import Data.Tuple (Tuple(..))
+import Lens.Types (Lens)
+import Undefined (undefined)
 
 {-
   中でlens'を呼び出している。
@@ -35,9 +37,14 @@ lens get set = lens' (\s -> (Tuple (get s) \b -> set s b))
   type Optic p s t a b = p a b -> p s t
 
   この定義を元に Lens s t a b の部分を展開するとこうなる
-  lens' :: forall p s t b a. Strong p => (s -> Tuple a (b -> t)) -> p a b -> p s t
+  lens' :: forall p s t a b. Strong p => (s -> Tuple a (b -> t)) -> p a b -> p s t
 
   こうすると2つ目の引数pabはまんま p a b であったことがわかる。
+
+  だから to だけでなく pab まで引数を渡したら p s t が返ってくるけど、pab まで渡さなかったら
+  p a b -> p s t が返ってくるわけ。
+  んで、`lens`は`to`しか渡してないので、p a b -> p s t すなわち Lens s t a b が返ってきている。
+
   ---------------------------------------------------------
   コードの解説
 
@@ -57,9 +64,23 @@ lens get set = lens' (\s -> (Tuple (get s) \b -> set s b))
   追っていくと、s は 最終的には t になるな。
   Tuple の a も途中で b になっている
 
-  `lens`の定義から、`get`と`set`が絡んでくるのは`to`の部分なので、上記の関数合成の部分に当てはめると
-  `get`と`set`が絡んでくるのは最初の部分だけなんだな。
+  [`lens`の`get`と`set`が、どのタイミングで呼ばれているか]
+  上記の関数合成の部分を見てみると、`get`は最初の関数の`(s -> Tuple a (b -> t))`の`a`の部分を取得する際に使われることがわかる。
+  また`set`は最初の関数`(s -> Tuple a (b -> t))`の`(b -> t)`の部分であるが、これは関数なので最初の関数が実行されるときは呼ばれない。
+  `(b -> t)`がどこで実行されるか追っていくと。関数合成の最後の関数`(\(Tuple b (b -> t)) -> (b -> t) b)`で使われている。
+  実装でいうと`(\(Tuple b f) -> f b)`の`f b`の部分。
+  なので`set`は関数合成の最後の関数で呼ばれる。
 
+  擬似的なコードだとこんな感じか。
+      (s -> Tuple (get s) (b -> t))
+  >>> ((Tuple a (b -> t)) -> (Tuple b (b -> t)))
+  >>> ((Tuple b (b -> t)) -> (\b -> set s b) b)
+
+  途中の関数は、`lens`や`lens'`で実装されている関数なので、関数の外側からコントロールできるのは
+  `get`, `set` と `pab` の部分となる。
+  `get`と`set`を指定して`Lens`を作ったあとは、`pab`で操作することになる。
+  なので結構この`pab`の部分はキモとなっていると思う。
+  `pab`の`first`を呼び出しているので、`first`もか。
 -}
 lens' :: forall s t a b. (s -> Tuple a (b -> t)) -> Lens s t a b
 lens' to pab = dimap to (\(Tuple b f) -> f b) (first pab)
