c11871261fac73661231ef96f9a96c6e2a005316
Pujo <pujoheadsoft@gmail.com>
2023-09-13 07:04:54 +0900

    wip

---
+++ misc/src/Aff/MyAff.js
@@ -0,0 +1,315 @@
+/* eslint-disable no-constant-condition */
+/* eslint-disable no-redeclare */
+/* globals setImmediate, clearImmediate, setTimeout, clearTimeout */
+/* eslint-disable no-unused-vars, no-prototype-builtins, no-use-before-define, no-unused-labels, no-param-reassign */
+var Aff = function () {
+  // A unique value for empty.
+  var EMPTY = {};
+
+  var PURE = "Pure";
+  var SYNC = "Sync";
+  var ASYNC = "Async";
+  var BIND = "Bind";
+  var FORK = "Fork";
+
+  // Various constructors used in interpretation
+  var CONS = "Cons";      // Cons-list, for stacks
+
+  function Aff(tag, _1, _2, _3) {
+    this.tag = tag;
+    this._1 = _1;
+    this._2 = _2;
+    this._3 = _3;
+  }
+
+  function AffCtr(tag) {
+    var fn = function (_1, _2, _3) {
+      return new Aff(tag, _1, _2, _3);
+    };
+    fn.tag = tag;
+    return fn;
+  }
+
+  function runEff(eff) {
+    try {
+      eff();
+    } catch (error) {
+      setTimeout(function () {
+        throw error;
+      }, 0);
+    }
+  }
+
+  function runSync(eff) {
+    return eff();
+  }
+
+  function runAsync(eff, k) {
+    return eff(k)();
+  }
+
+  var Scheduler = function () {
+    var limit = 1024;
+    var size = 0;
+    var ix = 0;
+    var queue = new Array(limit);
+    var draining = false;
+
+    function drain() {
+      var thunk;
+      draining = true;
+      while (size !== 0) {
+        size--;
+        thunk = queue[ix];
+        queue[ix] = void 0;
+        ix = (ix + 1) % limit;
+        thunk();
+      }
+      draining = false;
+    }
+
+    return {
+      isDraining: function () {
+        return draining;
+      },
+      enqueue: function (cb) {
+        var i, tmp;
+        if (size === limit) {
+          tmp = draining;
+          drain();
+          draining = tmp;
+        }
+
+        queue[(ix + size) % limit] = cb;
+        size++;
+
+        if (!draining) {
+          drain();
+        }
+      }
+    };
+  }();
+
+  // Fiber state machine
+  var SUSPENDED = 0; // Suspended, pending a join.
+  var CONTINUE = 1; // Interpret the next instruction.
+  var STEP_BIND = 2; // Apply the next bind.
+  var STEP_RESULT = 3; // Handle potential failure from a result.
+  var PENDING = 4; // An async effect is running.
+  var RETURN = 5; // The current stack has returned.
+  var COMPLETED = 6; // The entire fiber has completed.
+
+  function Fiber(aff) {
+    var runTick = 0;
+    var status = SUSPENDED;
+
+    var step = aff;  // Successful step
+
+    var bhead = null;
+    var btail = null;
+
+    function run(localRunTick) {
+      var tmp;
+      while (true) {
+        tmp = null;
+
+        switch (status) {
+          case STEP_BIND:
+            status = CONTINUE;
+            try {
+              step = bhead(step);
+              if (btail === null) {
+                bhead = null;
+              } else {
+                bhead = btail._1;
+                btail = btail._2;
+              }
+            } catch (e) {
+              status = RETURN;
+              step = null;
+            }
+            break;
+
+          case STEP_RESULT:
+            if (bhead === null) {
+              status = RETURN;
+            } else {
+              status = STEP_BIND;
+            }
+            break;
+
+          case CONTINUE:
+            switch (step.tag) {
+              case BIND:
+                if (bhead) {
+                  btail = new Aff(CONS, bhead, btail);
+                }
+                bhead = step._2;
+                status = CONTINUE;
+                step = step._1;
+                break;
+
+              case PURE:
+                if (bhead === null) {
+                  status = RETURN;
+                  step = step._1;
+                } else {
+                  status = STEP_BIND;
+                  step = step._1;
+                }
+                break;
+
+              case SYNC:
+                status = STEP_RESULT;
+                step = runSync(step._1);
+                break;
+
+              case ASYNC:
+                status = PENDING;
+                step = runAsync(step._1, function (result) {
+                  return function () {
+                    if (runTick !== localRunTick) {
+                      return;
+                    }
+                    runTick++;
+                    Scheduler.enqueue(function () {
+                      // It's possible to interrupt the fiber between enqueuing and
+                      // resuming, so we need to check that the runTick is still
+                      // valid.
+                      if (runTick !== localRunTick + 1) {
+                        return;
+                      }
+                      status = STEP_RESULT;
+                      step = result;
+                      run(runTick);
+                    });
+                  };
+                });
+                return;
+
+              case FORK:
+                status = STEP_RESULT;
+                tmp = Fiber(step._2);
+                if (step._1) {
+                  tmp.run();
+                }
+                step = tmp;
+                break;
+
+            }
+            break;
+
+          case RETURN:
+            bhead = null;
+            btail = null;
+            status = COMPLETED;
+            break;
+
+          case COMPLETED:
+            return;
+          case SUSPENDED:
+            status = CONTINUE;
+            break;
+          case PENDING: return;
+        }
+      }
+    }
+
+
+    return {
+      isSuspended: function () {
+        return status === SUSPENDED;
+      },
+      run: function () {
+        if (status === SUSPENDED) {
+          if (!Scheduler.isDraining()) {
+            Scheduler.enqueue(function () {
+              run(runTick);
+            });
+          } else {
+            run(runTick);
+          }
+        }
+      }
+    };
+  }
+
+
+  Aff.EMPTY = EMPTY;
+  Aff.Pure = AffCtr(PURE);
+  Aff.Sync = AffCtr(SYNC);
+  Aff.Async = AffCtr(ASYNC);
+  Aff.Bind = AffCtr(BIND);
+  Aff.Fork = AffCtr(FORK);
+  Aff.Fiber = Fiber;
+  Aff.Scheduler = Scheduler;
+
+  return Aff;
+}();
+
+export const _pure = Aff.Pure;
+
+export function _map(f) {
+  return function (aff) {
+    if (aff.tag === Aff.Pure.tag) {
+      return Aff.Pure(f(aff._1));
+    } else {
+      return Aff.Bind(aff, function (value) {
+        return Aff.Pure(f(value));
+      });
+    }
+  };
+}
+
+export function _bind(aff) {
+  return function (k) {
+    return Aff.Bind(aff, k);
+  };
+}
+
+export function _fork(immediate) {
+  return function (aff) {
+    return Aff.Fork(immediate, aff);
+  };
+}
+
+export const _liftEffect = Aff.Sync;
+
+export const makeAff = Aff.Async;
+
+export function _makeFiber(util, aff) {
+  return function () {
+    return Aff.Fiber(util, null, aff);
+  };
+}
+
+export const _delay = function () {
+  function setDelay(n, k) {
+    if (n === 0 && typeof setImmediate !== "undefined") {
+      return setImmediate(k);
+    } else {
+      return setTimeout(k, n);
+    }
+  }
+
+  function clearDelay(n, t) {
+    if (n === 0 && typeof clearImmediate !== "undefined") {
+      return clearImmediate(t);
+    } else {
+      return clearTimeout(t);
+    }
+  }
+
+  return function (right, ms) {
+    return Aff.Async(function (cb) {
+      return function () {
+        var timer = setDelay(ms, cb(right()));
+        return function () {
+          return Aff.Sync(function () {
+            return right(clearDelay(ms, timer));
+          });
+        };
+      };
+    });
+  };
+}();  
---
+++ misc/src/Aff/MyAff.purs
@@ -0,0 +1,145 @@
+module Aff.MyAff where
+
+import Prelude
+
+import Control.Alt (class Alt)
+import Control.Alternative (class Alternative)
+import Control.Apply (lift2)
+import Control.Lazy (class Lazy)
+import Control.Monad.Error.Class (class MonadError, class MonadThrow, throwError, catchError, try)
+import Control.Monad.Error.Class (try, throwError, catchError) as Exports
+import Control.Monad.Rec.Class (class MonadRec, Step(..))
+import Control.Monad.ST.Class (class MonadST, liftST)
+import Control.Monad.ST.Global (Global)
+import Control.Parallel (parSequence_, parallel)
+import Control.Parallel.Class (class Parallel)
+import Control.Parallel.Class (sequential, parallel) as Exports
+import Control.Plus (class Plus, empty)
+import Data.Either (Either(..))
+import Data.Function.Uncurried as Fn
+import Data.Newtype (class Newtype)
+import Data.Time.Duration (Milliseconds(..))
+import Data.Time.Duration (Milliseconds(..)) as Exports
+import Effect (Effect)
+import Effect.Class (class MonadEffect, liftEffect)
+import Effect.Exception (Error, error)
+import Effect.Exception (Error, error, message) as Exports
+import Effect.Unsafe (unsafePerformEffect)
+import Partial.Unsafe (unsafeCrashWith)
+import Unsafe.Coerce (unsafeCoerce)
+
+-- | An `Aff a` is an asynchronous computation with effects. The
+-- | computation may either error with an exception, or produce a result of
+-- | type `a`. `Aff` effects are assembled from primitive `Effect` effects using
+-- | `makeAff` or `liftEffect`.
+foreign import data Aff :: Type -> Type
+
+type role Aff representational
+
+instance functorAff :: Functor Aff where
+  map = _map
+
+instance applyAff :: Apply Aff where
+  apply = ap
+
+instance applicativeAff :: Applicative Aff where
+  pure = _pure
+
+instance bindAff :: Bind Aff where
+  bind = _bind
+
+instance monadAff :: Monad Aff
+
+instance semigroupAff :: Semigroup a => Semigroup (Aff a) where
+  append = lift2 append
+
+instance monoidAff :: Monoid a => Monoid (Aff a) where
+  mempty = pure mempty
+
+
+-- | This instance is provided for compatibility. `Aff` is always stack-safe
+-- | within a given fiber. This instance will just result in unnecessary
+-- | bind overhead.
+instance monadRecAff :: MonadRec Aff where
+  tailRecM k = go
+    where
+    go a = do
+      res <- k a
+      case res of
+        Done r -> pure r
+        Loop b -> go b
+
+instance monadEffectAff :: MonadEffect Aff where
+  liftEffect = _liftEffect
+
+instance lazyAff :: Lazy (Aff a) where
+  defer f = pure unit >>= f
+
+instance monadSTAff :: MonadST Global Aff where
+  liftST = liftST >>> liftEffect
+
+type OnComplete a =
+  { rethrow :: Boolean
+  , handler :: (Either Error a -> Effect Unit) -> Effect Unit
+  }
+
+-- | Represents a forked computation by way of `forkAff`. `Fiber`s are
+-- | memoized, so their results are only computed once.
+newtype Fiber a = Fiber
+  { run :: Effect Unit
+  , isSuspended :: Effect Boolean
+  }
+
+-- | Forks an `Aff` from an `Effect` context, returning the `Fiber`.
+launchAff :: forall a. Aff a -> Effect (Fiber a)
+launchAff aff = do
+  fiber <- makeFiber aff
+  case fiber of Fiber f -> f.run
+  pure fiber
+
+-- | Forks an `Aff` from an `Effect` context, discarding the `Fiber`.
+launchAff_ :: Aff Unit -> Effect Unit
+launchAff_ = void <<< launchAff
+
+-- | Suspends an `Aff` from an `Effect` context, returning the `Fiber`.
+launchSuspendedAff :: forall a. Aff a -> Effect (Fiber a)
+launchSuspendedAff = makeFiber
+
+-- | Forks an `Aff` from an `Effect` context and also takes a callback to run when
+-- | it completes. Returns the pending `Fiber`.
+runAff :: forall a. (Either Error a -> Effect Unit) -> Aff a -> Effect (Fiber Unit)
+runAff k aff = launchAff $ liftEffect <<< k =<< try aff
+
+-- | Forks an `Aff` from an `Effect` context and also takes a callback to run when
+-- | it completes, discarding the `Fiber`.
+runAff_ :: forall a. (Either Error a -> Effect Unit) -> Aff a -> Effect Unit
+runAff_ k aff = void $ runAff k aff
+
+
+-- | Forks an `Aff` from within a parent `Aff` context, returning the `Fiber`.
+forkAff :: forall a. Aff a -> Aff (Fiber a)
+forkAff = _fork true
+
+-- | Suspends an `Aff` from within a parent `Aff` context, returning the `Fiber`.
+-- | A suspended `Aff` is not executed until a consumer observes the result
+-- | with `joinFiber`.
+suspendAff :: forall a. Aff a -> Aff (Fiber a)
+suspendAff = _fork false
+
+-- | Pauses the running fiber.
+delay :: Milliseconds -> Aff Unit
+delay (Milliseconds n) = Fn.runFn2 _delay Right n
+
+foreign import _pure :: forall a. a -> Aff a
+foreign import _fork :: forall a. Boolean -> Aff a -> Aff (Fiber a)
+foreign import _map :: forall a b. (a -> b) -> Aff a -> Aff b
+foreign import _bind :: forall a b. Aff a -> (a -> Aff b) -> Aff b
+foreign import _delay :: forall a. Fn.Fn2 (Unit -> Either a Unit) Number (Aff Unit)
+foreign import _liftEffect :: forall a. Effect a -> Aff a
+foreign import _makeFiber :: forall a. Fn.Fn1 (Aff a) (Effect (Fiber a))
+
+foreign import makeAff :: forall a. ((a -> Effect Unit) -> Effect Unit) -> Aff a
+
+makeFiber :: forall a. Aff a -> Effect (Fiber a)
+makeFiber aff = Fn.runFn1 _makeFiber aff
+
