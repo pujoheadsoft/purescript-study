0943fa75dfe4f23904ca3b1d470d873e165dce77
Pujo <pujoheadsoft@gmail.com>
2023-11-18 19:23:16 +0900

    add comment

--- misc/src/TaglessFinal/Gateway/Gateway.purs
+++ misc/src/TaglessFinal/Gateway/Gateway.purs
@@ -20,3 +20,18 @@ createArticlePortFunction = {
     pure {title: json.title}
 }
 
+{-
+  MonadAff を ArticlePortのインスタンスにしようとすると Orphan instance でコンパイルエラーになってしまう
+  更にnewtypeでMonadAffのラッパーは作れない。
+
+  なぜならばnewtypeは新しい型を定義するために使われるが
+  MonadAff mが具体的な型ではなく制約を表す型クラスであるため。
+-}
+-- instance instancePort :: MonadAff m => ArticlePort m where
+--   findByTitle title = do
+--     index <- findIndexByTitle title
+--     json <- findJsonById index.id
+--     pure {title: json.title}
+
+-- これはできない
+-- newtype MonadAffWrapper = MonadAffWrapper (MonadAff m)
