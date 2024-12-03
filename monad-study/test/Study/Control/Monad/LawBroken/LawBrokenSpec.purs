88392e34c14ccb63aed07463b1abf82c2560b992
Pujo <pujoheadsoft@gmail.com>
2024-12-04 07:20:57 +0900

    refactor

--- monad-study/test/Study/Control/Monad/LawBroken/LawBrokenSpec.purs
+++ monad-study/test/Study/Control/Monad/LawBroken/LawBrokenSpec.purs
@@ -1,7 +1,7 @@
 module Test.Study.Control.Monad.LawBroken.LawBrokenSpec where
 
 import Prelude
-import Study.Control.Monad.LawBroken.LawBroken
+import Study.Control.Monad.LawBroken.LawBroken (broken, value)
 
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions ( shouldNotEqual)
@@ -11,8 +11,8 @@ spec = do
   describe "Monad則" do
     it "壊れてること" do
       let 
-        -- (m >>= id) >>= id
+        -- (m >>= f) >>= g
         left = (broken 1 *> broken 2) *> broken 3
-        -- m >>= (\x -> id x >>= id)
+        -- m >>= (\x -> f x >>= g)
         right = broken 1 *> (broken 2 *> broken 3)
       value left `shouldNotEqual` value right
